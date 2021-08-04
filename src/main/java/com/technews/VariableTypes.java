2847ea836803e947de7fb4350bcad17e00d005d0
RichA <rich_ay@sbcglobal.net>
2021-08-04 16:09:25 -0500

    Uploading test/demo JAVA introductory files.

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,16 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+
+        int i = 10;
+        double dd = 5.5;
+        double sum = dd + i;
+
+        String s = "The value of i is: ";
+        String o = i + s;
+
+        System.out.println( o);
+        System.out.println( sum );
+    }
+}
