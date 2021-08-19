2df8af626fda7a6f687421e8072d0907002a2ea7
RichA <rich_ay@sbcglobal.net>
2021-08-19 16:53:28 -0500

    Updated 'post.java' and 'application.properties'.

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -1,6 +1,5 @@
 package com.technews.model;
 
-// See 'User.java' for important comments as the the structure and use of the code below.
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
@@ -13,40 +12,37 @@ import java.util.Objects;
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "post")
-
 public class Post implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-
     private String title;
     private String postUrl;
-
     @Transient
     private String userName;
-
     @Transient
     private int voteCount;
-
     private Integer userId;
-
-    @NotNull                               // can't be null in the database
-    @Temporal(TemporalType.DATE)           // allows the use of the type 'Date' in the database
-    @Column(name = "posted_at")            // the name of the column in the database
-    private Date postedAt = new Date();
 
     @NotNull
     @Temporal(TemporalType.DATE)
     @Column(name = "posted_at")
+    private Date postedAt = new Date();
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name = "updated_at")
     private Date updatedAt = new Date();
 
     // Need to use FetchType.LAZY to resolve multiple bags exception
     @OneToMany(mappedBy = "postId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Comment> comments;
 
+
     public Post() {
     }
+
     public Post(Integer id, String title, String postUrl, int voteCount, Integer userId) {
         this.id = id;
         this.title = title;
@@ -54,60 +50,79 @@ public class Post implements Serializable {
         this.voteCount = voteCount;
         this.userId = userId;
     }
+
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getPostUrl() {
         return postUrl;
     }
+
     public void setPostUrl(String postUrl) {
         this.postUrl = postUrl;
     }
+
     public String getUserName() {
         return userName;
     }
+
     public void setUserName(String userName) {
         this.userName = userName;
     }
+
     public int getVoteCount() {
         return voteCount;
     }
+
     public void setVoteCount(int voteCount) {
         this.voteCount = voteCount;
     }
+
     public Integer getUserId() {
         return userId;
     }
+
     public void setUserId(Integer userId) {
         this.userId = userId;
     }
+
     public Date getPostedAt() {
         return postedAt;
     }
+
     public void setPostedAt(Date postedAt) {
         this.postedAt = postedAt;
     }
+
     public Date getUpdatedAt() {
         return updatedAt;
     }
+
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
     }
+
     public List<Comment> getComments() {
         return comments;
     }
+
     public void setComments(List<Comment> comments) {
         this.comments = comments;
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -123,10 +138,12 @@ public class Post implements Serializable {
                 Objects.equals(getUpdatedAt(), post.getUpdatedAt()) &&
                 Objects.equals(getComments(), post.getComments());
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(getId(), getTitle(), getPostUrl(), getUserName(), getVoteCount(), getUserId(), getPostedAt(), getUpdatedAt(), getComments());
     }
+
     @Override
     public String toString() {
         return "Post{" +
